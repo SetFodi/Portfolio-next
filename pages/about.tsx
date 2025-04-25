573cf9bf991a82f5939e73b20c7223e8ea17d4b6
SetFodi <lukafartenadze2004@gmail.com>
2025-04-26 00:44:27 +0400

    removed unused link for vercel

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import Link from 'next/link';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { 
   FaFacebook, 
